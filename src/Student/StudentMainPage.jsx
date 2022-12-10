2efab261788966478b34b6ddf90acccde8b413de
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-12-10 20:26:12 +0300

    First Commit

--- src/Student/StudentMainPage.jsx
+++ src/Student/StudentMainPage.jsx
@@ -3,31 +3,32 @@ import Navbar from './Navbar';
 import NavPage from './NavPage';
 import Sidebar from './Sidebar';
 import './App2.css'
-<<<<<<< HEAD
+ HEAD
 function StudentMainPage({user}) {
     if (user) {
       return <h1>Welcome, {user.username}!</h1>;
     }
-=======
-function StudentMainPage() {
->>>>>>> bcbc8bec4fce633e652e844ba2912aaea3bae2a0
-  return (
-    <>
-      {/* navbar section */}
-      <div>
-        <Navbar/>
+  }
 
-      </div>
-      {/* sidebar section */}
-      <div className='sidebar-container'>
-        <div className='side-bar'>
-          <Sidebar/>
-        </div>
-        <div className='nav-page'>
-          <NavPage/>
-        </div>
-      </div>
-    </>
-  )
-}
+// function StudentMainPage() {
+//  bcbc8bec4fce633e652e844ba2912aaea3bae2a0
+//   return (
+//     <>
+//       {/* navbar section */}
+//       <div>
+//         <Navbar/>
+
+//       </div>
+//       {/* sidebar section */}
+//       <div className='sidebar-container'>
+//         <div className='side-bar'>
+//           <Sidebar/>
+//         </div>
+//         <div className='nav-page'>
+//           <NavPage/>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
 export default StudentMainPage;
